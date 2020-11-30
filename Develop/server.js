d9933ed3f78fd6e09ec548a90cde1ce946d50fb5
Keith Fung <fungsenkeith@gmail.com>
2020-11-29 22:08:51 -0500

    made dynamic ports

--- Develop/server.js
+++ Develop/server.js
@@ -1,27 +1,32 @@
+// Global Dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = 3000;
-
+// Establish PORT Connection
+const PORT = process.env.PORT || 3000;
 const app = express();
 
+// Middleware
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Set static files to public folder
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+// Mongoose connect (workout database)
+// https://mongoosejs.com/docs/deprecations.html
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
-});
+}); // Following initial setup in seed.js
 
-// routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
+// Routes
+app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes.js"));
 
+// Initiate server
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`Currently hosted on port ${PORT}!`);
 });
